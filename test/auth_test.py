bfaa8ea86514daee9d16f82e1da5afb28669ec01
rocaltair <rocaltair@gmail.com>
2018-04-24 12:55:46 +0800

    add queries param to signature

---
+++ test/auth_test.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import fc2
+import unittest
+
+
+class TestAuth(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestAuth, self).__init__(*args, **kwargs)
+
+    def test_get_sign_resource(self):
+        queries = {
+            'key3': [],
+            'key2': ['123'],
+            'foo': 'bar',
+            'key4-with /escaped~chars_here.ext':'value-with /escaped~chars_here.ext',
+            'key1': ['xyz', 'abc'],
+        }
+        sign_resource = fc2.auth.Auth._get_sign_resource('/path/action with-escaped~chars_here.ext', queries)
+        self.assertEqual(sign_resource, '/path/action with-escaped~chars_here.ext\nfoo=bar\nkey1=abc\nkey1=xyz\nkey2=123\nkey3\nkey4-with /escaped~chars_here.ext=value-with /escaped~chars_here.ext')
+
+if __name__ == '__main__':
+    unittest.main()
