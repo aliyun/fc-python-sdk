00fed317c6a67c05ff4caf39afbb166a639ea3e6
rsonghuster <ls_huster@163.com>
2022-09-21 15:48:12 +0800

    release 2.5.2

--- fc2/__init__.py
+++ fc2/__init__.py
@@ -6,7 +6,7 @@ https://www.aliyun.com/product/fc
 """
 
 __author__ = 'Aliyun Function Compute'
-__version__ = '2.5.0'
+__version__ = '2.5.2'
 
 from .client import Client
 from .fc_exceptions import FcError
